b6eceb51690beebcb7093b4963067005822361fc
Sabin Marcu <sabinmarcu@gmail.com>
2015-12-28 15:36:22 +0200

    possible fix for build (older nodejs)

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ var TransferWebpackPlugin = require('transfer-webpack-plugin');
 var devFlagPlugin = new webpack.DefinePlugin({
     __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false')),
     __CHROMEAPP__: JSON.stringify(JSON.parse((process.env.ENV || "browser").toLowerCase() === "chrome" || 'false')),
-    __NAME__: `"${(process.env.SITENAME || "AppNameHere")}"`,
-    __BASEURL__: `"${(process.env.BASEURL || "/")}"`,
+    __NAME__: "\"" + (process.env.SITENAME || "AppNameHere") + "\"",
+    __BASEURL__: "\"" + (process.env.BASEURL || "/") + "\"",
 });
 
 var config = {
